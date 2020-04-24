32da0818179b4a78736d186d8faf5933fe331d20
Ramananda Pokkula <rpokkula@MacBook-Pro.local>
2020-04-24 15:08:29 +0100

    Update script to use Clock module

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,34 @@
 import boto3
+import sys
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 Instances"
+    for i in  ec2.instances.all():
+        print(', '.join((
+           i.id,
+           i.instance_type,
+           i.placement['AvailabilityZone'],
+           i.state['Name'],
+           i.public_dns_name)))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
+    list_instances()
 
-    for i in  ec2.instances.all():
-        print(i)
+
+# shotty list instances
+# shotty instances list
+# shotty snapshot instances
+# shotty instances shapshot
+# shotty instances create-snapshots
+# shotty list volumes
+# shotty volumes list_instances
+# shotty instances stop tag=Project:valkyre
+# shotty start instances --project=valkyre
+# shotty instances stop
